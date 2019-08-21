a4eb15d1ac6a16f52dbeb7ed11a5afc7b6faef3f
tianke <tianke645@163.com>
2019-08-21 18:06:52 +0800

    update 用户icon

--- 学比邻老师客户端/ViewModels/UserProfileViewModel.cs
+++ 学比邻老师客户端/ViewModels/UserProfileViewModel.cs
@@ -10,6 +10,13 @@ namespace 学比邻老师客户端.ViewModels
     public class UserProfileViewModel : BaseViewModel
     {
         public new string DisplayName => "我的";
+       
+        // 用户名
+        public string Username { get; set; }
+
+        // 邮箱
+        public string Email { get; set; }
+
 
     }
 }
