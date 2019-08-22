5c9a75aa59feb3caa26da1e340a06065f9921368
tianke <tianke645@163.com>
2019-08-22 20:55:07 +0800

    update user

---
+++ 学比邻老师客户端/Services/UserService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Anotar.NLog;
+using Fody;
+using 学比邻老师客户端.ViewModels;
+
+namespace 学比邻老师客户端.Services
+{
+    [ConfigureAwait(false)]
+    public class UserService
+    {
+        public async Task<(int, UserProfile)> GetUserProfile(string token)
+        {
+            try
+            {
+                return await Task.Run(() =>
+                {
+                    Thread.Sleep(2500);
+
+                    return (0, new UserProfile() {
+                        Email = "tianke645@163.com",
+                        Username = "tianke"
+                        
+                    });
+                });
+            }
+            catch (Exception e)
+            {
+                LogTo.InfoException(e.Message, e);
+                return (-1, null);
+            }
+        }
+
+
+        
+    }
+}
--- 学比邻老师客户端/ViewModels/UserProfileViewModel.cs
+++ 学比邻老师客户端/ViewModels/UserProfileViewModel.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Stylet;
+using StyletIoC;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using 学比邻老师客户端.Services;
 using 学比邻老师客户端.ViewModels.@base;
 
 namespace 学比邻老师客户端.ViewModels
@@ -10,13 +13,49 @@ namespace 学比邻老师客户端.ViewModels
     public class UserProfileViewModel : BaseViewModel
     {
         public new string DisplayName => "我的";
-       
+
+        [Inject] UserService UserService;
+
+        public UserProfile UserProfile { get; set; }
+
+
+        private void DoLoad()
+        {
+            UserService.GetUserProfile("token").ContinueWith(res =>
+           {
+               var (code, data) = res.Result;
+
+               if (code == 0)
+               {
+                   UserProfile = data;
+               }
+               else
+               {
+                   Execute.OnUIThread(() =>
+                   {
+ 
+                   });
+               }
+           });
+        }
+
+        protected override void OnViewLoaded()
+        {
+            base.OnViewLoaded();
+
+            DoLoad();
+
+        }
+
+    }
+
+
+    public class UserProfile: PropertyChangedBase
+    {
         // 用户名
         public string Username { get; set; }
 
         // 邮箱
         public string Email { get; set; }
-
-
     }
 }
